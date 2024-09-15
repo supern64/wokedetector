14dd5de81a325294a587eb23ce61634b50559ff9
SuperN64 <n64@cirnoslab.me>
2024-09-15 15:35:47 +0700

    add environment variables

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # cronjob script for periodically updating the list
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+export NVM_DIR="/home/node/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
 cd /var/www/wokedetector/current
 pm2 stop ecosystem.config.cjs
 bun run build
