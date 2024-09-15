f605ef041f437e5d1bc3761c9f2c2a2a49991a19
SuperN64 <n64@cirnoslab.me>
2024-09-15 12:13:23 +0700

    nyeh

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -18,8 +18,9 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:supern64/wokedetector.git',
       path: '/var/www/wokedetector',
+      
       'pre-deploy-local': '',
-      'post-deploy' : '/home/node/.bun/bin/bun install && /home/node/.bun/bin/bun run build && pm2 reload ecosystem.config.cjs --env production',
+      'post-deploy' : '/home/node/.bun/bin/bun install && ls -la && /home/node/.bun/bin/bun run build && pm2 reload ecosystem.config.cjs --env production',
       'pre-setup': '',
       ssh_options: "StrictHostKeyChecking=no",
     }
