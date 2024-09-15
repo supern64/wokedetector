34fa7df05989d3aca29e6d996405bd1b9526d38d
SuperN64 <n64@cirnoslab.me>
2024-09-15 15:09:35 +0700

    update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # cronjob script for periodically updating the list
-cd /var/www/wokedetector
+cd /var/www/wokedetector/current
 bun run build
 pm2 reload ecosystem.config.cjs --env production
