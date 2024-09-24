b6ccc3fe19c80ab296d502b98b60c33b71e16e06
SuperN64 <chanon-tong@hotmail.com>
2024-09-25 06:15:47 +0700

    Update +page.svelte

--- src/routes/full-list/+page.svelte
+++ src/routes/full-list/+page.svelte
@@ -41,6 +41,6 @@
 
 <footer>
     list last updated on {dayjs(data.lastUpdate).toDate().toLocaleString()} ({dayjs(data.lastUpdate).fromNow()})<br>
-    *i am not associated with the woke content detector steam group, nor do i endorse any of the comments in it. this website was made as a joke.
+    *this list was compiled from the woke content detector steam group, but i am not associated with it, nor do i endorse any of the comments it makes. this website was made as a joke.
 </footer>
 
